6dc0fdd20f5a298a8db3fbaffb48b96fecb2cfe9
lisa scanzi <lisascanzi@gmail.com>
2022-09-07 18:05:33 +0200

    fini pour aujourd hui

---
+++ clone-whatsapp-front/src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import './main.css'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  securedAxiosInstance,
+  plainAxiosInstance,
+  components: { App },
+  template: '<App/>'
+})
---
+++ clone-whatsapp-front/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/',
+      name: 'Signup',
+      component: Signup
+    }
+  ]
+})
